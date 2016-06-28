bc93cea0e9b356ef5b842bc907357906c015fb96
Joshua Mabina <mabinajoshua@gmail.com>
2016-06-28 05:01:06 +0300

    Fix merge conflicts w/ html-scaffold

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,21 @@
-/**
- * app.js
+/*!
+ * iamjoshuamabina.github.io
  *
- * This file compiles to /public/assets/js/app.min.js
- * You should use it build your js rainbows.
+ * @Author Joshua Mabina <mabinajoshua@gmail.com>
+ * @License WTFPL <wtfpl.net>
  */
 
-<<<<<<< HEAD
-function doSomething() {
-    "use strict";
-
-<<<<<<< HEAD
-    var results = [];
-    for(var i = 0; i < 12; i++) {
-       results[i] = getRandomNumber(1, 13) * (i + 1);
-    }
-=======
-    $('#dateDiv').html(getDate());
->>>>>>> added gulp lint task
-}
-
-function getRandomNumber(max, min) {
-    "use strict";
-
-    var randomNumber = Math.random();
-    return randomNumber * (max - min) + min;
-}
-=======
 $(function() {
-    "use strict";
+	"use strict";
 
-    function quotes() {
-        $('.carousel').carousel({
-            interval: 5000,
-            wrap: true
-        });
-    }
+	function quotes() {
+		console.log("Hello world! I am Joshua Mabina");
+	}
 
-    function init() {
-        quotes();
-    }
+	function init() {
+		quotes();
+	}
 
-    init();
+	init();
 
 });
->>>>>>> added carousel quotes
